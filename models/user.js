5f1b11064520211ea7ff66d3f71c81b851b7b2b1
Matt Logan <mlogan1313@gmail.com>
2018-02-06 11:24:08 -0600

    completed boilerplate

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
+
+//define model
+const userSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: String
+});
+
+// on save hook, encrypt password
+userSchema.pre("save", function(next) {
+  const user = this;
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, isMatch);
+  });
+};
+
+//create model class
+const ModelClass = mongoose.model("user", userSchema);
+
+//export
+module.exports = ModelClass;
